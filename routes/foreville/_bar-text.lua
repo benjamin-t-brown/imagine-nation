55542b9119c0162b93e350a002502b44879f4fc6
minjammben <minjammben@hotmail.com>
2016-02-16 14:33:50 -0700

    changed ignore

--- routes/foreville/_bar-text.lua
+++ routes/foreville/_bar-text.lua
@@ -2,7 +2,8 @@ return function()
 return {
  [ 1 ] = { [[  You step into a pristine bar. It is well-lit, clean-smelling, and all
   of the chairs and tables are neatly aligned. Yet the only person in
-  here is a man who stands vigilant at the counter, staring at you.]] }
+  here is a man who stands vigilant at the counter. He considers you,
+  but he seems to be waiting for you to speak to him.]] }
 ,[ 2 ] = { [[]] }
 } 
 end
